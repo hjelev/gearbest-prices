f5eed446a7854c36515760511075ee4d2336f256
Pavel Zamyatin <pavel@dotlovesdata.com>
2018-06-15 09:17:30 +1200

    Disabled logger

--- tracker/get_old_price.py
+++ tracker/get_old_price.py
@@ -12,13 +12,14 @@ def get_old_price(db_dir,link,stock_lvl,price): # reads old data and updates it 
 	db_filename = db_dir + link + ".txt"
 
 	if os.path.isfile(db_filename):  # check if file exists
-		log.info('File exist, writing into %s' % db_filename)
+		# log.info('File exist, writing into %s' % db_filename)
 		fo = open(db_filename, "r+")
 		line = fo.readline() # if there is a file read the previous value
 		old_price = line.split(",")[0]
 		stock_avail = line.split(",")[1]
 		
 	else:
+		# log.info('File doesn't exist, creating missing file: %s' % db_filename)
 		fo = open(db_filename, "w+") # create the missing file
 		old_price = "0" # set previous price to 0
 		stock_avail = "Unknown"
