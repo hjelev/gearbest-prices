1f5cd1f1061e1cc23c2e94dfea0f3d6a0dfa967e
Pavel Zamyatin <pavel@dotlovesdata.com>
2018-06-15 09:17:18 +1200

    Removed settings parser

--- gearbest-prices.py
+++ gearbest-prices.py
@@ -3,9 +3,6 @@
 import os.path, configparser
 from tracker.price_check import price_check
 
-config = configparser.ConfigParser()
-config.read('tracker/config.ini')
-
 url_list = []
 url_list.append("http://www.gearbest.com/batteries-chargers/pp_140785.html")  #usb led light
 url_list.append("http://www.gearbest.com/electronics-gadgets/pp_280338.html") # enhanced xiaomi led light
