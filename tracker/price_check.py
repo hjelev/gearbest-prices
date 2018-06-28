1bdb8880a818c5148aafcd1eb04432f7d560b9d2
Pavel Zamyatin <pavel@dotlovesdata.com>
2018-06-28 17:37:53 +1200

    Re-wrote send module

--- tracker/price_check.py
+++ tracker/price_check.py
@@ -1,5 +1,7 @@
+import os
+
 from tracker.parse_url import parse_url
-from tracker.send_message import send_message
+from tracker.send_email import send_email
 from tracker.get_old_price import get_old_price
 from tracker.config import config
 
@@ -20,22 +22,22 @@ def price_check(check_url):
     # NEED TO FIX. UGLY AS HELL!
     price = round(float(price)*float(RATE),2)
     old_price = round(float(old_price.split(",")[0])*float(RATE),2)
+    product_name = product_name[:60]
 
-    print('')
-    print('************** [ITEM] **************')
-    print("Name: %s~" % product_name[:60])
-    print("URL: %s" % check_url)
-    print("New price: ${0}{2} / Old price: ${1}{2}".format(price, old_price, CURRENCY))
+    log.info('************** [ITEM] **************')
+    log.info("Name: %s~" % product_name)
+    log.info("URL: %s" % check_url)
+    log.info("New price: ${0}{2} / Old price: ${1}{2}".format(price, old_price, CURRENCY))
     
     change = False
     body = ''
 
     # checking availability
     if stock_avail == stock_lvl+"\n":  #check for stock level change 
-        print("Stock: No changes")
+        log.info("Stock: No changes")
     else:
         change = True
-        print("Stock: Availability changed to '%s'!" % stock_lvl)
+        log.info("Stock: Availability changed to '%s'!" % stock_lvl)
 
     # checking price changes
     if price == old_price:
@@ -43,14 +45,27 @@ def price_check(check_url):
     else:
         trend = 'raised' if price > old_price else 'dropped'
         change = True
-        body += "PRICE | New price: %s The price have %s with %s\nAvailability: %s\n" % (price, trend, float(price) - float(old_price), stock_lvl)
+        diff = float(price) - float(old_price)
+        body += "Price: New price - $%s. The price has %s to $%s\nAvailability: %s\n" % (price, trend, diff, stock_lvl)
 
     # sending a message
-    if (change and old_price != 0):
-        log.debug('***DEBUG. Message will be sent.***')      
-        header = "<h2>%s</h2>\n" % product_name
-        footer = "<a href='%s'></a>" % check_url
+    # if (change and old_price != 0):
+    if (change):
+
+        conf = config['SMTP']
+        header = "Product: %s\n" % product_name
+        footer = "Link: %s" % check_url
         message = header + body + footer
-        # send_message(message, product_name)
+
+        log.info('Sending an email. Please wait a second...')
+
+        success = send_email(
+            server=conf['server'],
+            port=conf['port'],
+            subject='GB Prices Notification | Price or stock on one of the items in your list has been changed',
+            message=message
+        )
+
+        log.info('Email has been sent.') if success else log.error('Something went wrong. Please check the logs.')
             
     return
